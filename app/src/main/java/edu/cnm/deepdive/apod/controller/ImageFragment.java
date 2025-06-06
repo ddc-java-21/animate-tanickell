14e5a99dd0a35c069ad499ea699e28ae81728b27
tanickell <tnickell1@cnm.edu>
2025-06-06 12:03:35 -0600

    Started file download using Retrofit & ResponseBody.

--- app/src/main/java/edu/cnm/deepdive/apod/controller/ImageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/apod/controller/ImageFragment.java
@@ -1,9 +1,16 @@
 package edu.cnm.deepdive.apod.controller;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Images;
+import android.provider.MediaStore.Images.Media;
+import android.provider.MediaStore.MediaColumns;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -25,10 +32,14 @@ import edu.cnm.deepdive.apod.R;
 import edu.cnm.deepdive.apod.databinding.FragmentImageBinding;
 import edu.cnm.deepdive.apod.model.Apod;
 import edu.cnm.deepdive.apod.viewmodel.ApodViewModel;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class ImageFragment extends Fragment implements MenuProvider {
 
   private FragmentImageBinding binding;
+  private Apod apod;
 
   @Nullable
   @Override
@@ -72,6 +83,7 @@ public class ImageFragment extends Fragment implements MenuProvider {
   }
 
   private void displayApod(Apod apod) {
+    this.apod = apod;
     //noinspection DataFlowIssue
     ((AppCompatActivity) requireActivity())
         .getSupportActionBar() // we know we have an action bar, so even though it's nullable, we'll be ok
@@ -99,6 +111,47 @@ public class ImageFragment extends Fragment implements MenuProvider {
       // TODO: 6/4/25 Handle as appropriate.
     }
 
+  }
+
+  private class GalleryImageFinalizer implements Target {
+
+    private final Apod apod;
+
+    private GalleryImageFinalizer(Apod apod) {
+      this.apod = apod;
+    }
+
+
+    @Override
+    public void onBitmapLoaded(Bitmap bitmap, LoadedFrom loadedFrom) {
+      ContentValues values = new ContentValues();
+      values.put(MediaColumns.DISPLAY_NAME, apod.getTitle().strip());
+      values.put(MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES + "/APOD");
+      values.put(MediaColumns.IS_PENDING, 1);
+
+      ContentResolver resolver = requireContext().getContentResolver();
+      Uri uri = resolver.insert(Media.EXTERNAL_CONTENT_URI, values);
+      if (uri != null) {
+        try (OutputStream out = resolver.openOutputStream(uri)) {
+
+        } catch (IOException e) {
+          throw new RuntimeException(e); // FIXME: 6/6/25 Notify user of failure.
+        }
+      } else {
+        // We should never get here!
+        // FIXME: 6/6/25 Notify user of failure.
+      }
+    }
+
+    @Override
+    public void onBitmapFailed(Exception e, Drawable drawable) {
+      // TODO: 6/6/25 Display a snackbar to user, indicating failure.
+    }
+
+    @Override
+    public void onPrepareLoad(Drawable drawable) {
+      // TODO: 6/6/25 Obtain URIs, random filenames, etc.
+    }
   }
 
 }
