f40d14f48765ef63a8c1b3a69dc5d3a3bbee68fe
tanickell <tnickell1@cnm.edu>
2025-07-10 15:46:14 -0600

    First draft database successfully generated.

--- app/src/main/java/edu/cnm/deepdive/animate/AnimateApplication.java
+++ app/src/main/java/edu/cnm/deepdive/animate/AnimateApplication.java
@@ -1,10 +1,13 @@
 package edu.cnm.deepdive.animate;
 
 import android.app.Application;
+import androidx.room.RoomDatabase;
 import com.squareup.picasso.Picasso;
 import dagger.hilt.android.HiltAndroidApp;
+import edu.cnm.deepdive.animate.service.AnimateDatabase;
 import edu.cnm.deepdive.animate.service.AnimeProxy;
 import edu.cnm.deepdive.animate.service.AnimeService;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 @HiltAndroidApp
 public class AnimateApplication extends Application {
@@ -15,6 +18,13 @@ public class AnimateApplication extends Application {
     AnimeProxy.setContext(this);
     AnimeService.setContext(this);
     Picasso.setSingletonInstance(new Picasso.Builder(this).build());
+
+//    new RoomDatabase.Builder<>(this, AnimateDatabase.class, "animate-db")
+//        .build()
+//        .getAnimeDao()
+//        .deleteAll()
+//        .subscribeOn(Schedulers.io())
+//        .subscribe();
   }
 
 }
